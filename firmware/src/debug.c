6877b2b93d1aa047422f3e556eec31ab603a3037
Manuel Kasper <mk@neon1.net>
2025-12-07 15:06:34 +0100

    Cleanup debug macro

--- firmware/src/debug.c
+++ firmware/src/debug.c
@@ -12,14 +12,12 @@
 #ifdef DEBUG
 static int uart_putchar(char c, FILE *stream);
 static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
-#endif
 
 static volatile uint8_t tx_buf[DEBUG_BUFFER_SIZE];
 static volatile uint8_t tx_head = 0;
 static volatile uint8_t tx_tail = 0;
 
 void debug_init(void) {
-#ifdef DEBUG
     PORTMUX.USARTROUTEA |= PORTMUX_USART0_ALT1_gc; // Use alternate pins for USART0 (PA1=TX, PA2=RX)
     PORTA.DIRSET = PIN1_bm; // Set TX pin as output
     USART0.BAUD = (uint16_t)USART_BAUD_RATE(115200); /* set baud rate register */ 
@@ -29,22 +27,16 @@ void debug_init(void) {
     USART0.CTRLC = USART_CMODE_ASYNCHRONOUS_gc | USART_CHSIZE_8BIT_gc | USART_PMODE_DISABLED_gc | USART_SBMODE_1BIT_gc;
 
     stdout = &mystdout;		// define the output stream
-#endif
 }
 
 char debug_read_char(void) {
-#ifdef DEBUG
     if (USART0.STATUS & USART_RXCIF_bm) {
         return USART0.RXDATAL;
     } else {
         return 0;
     }
-#else
-    return 0;
-#endif
 }
 
-#ifdef DEBUG
 static int uart_putchar(char c, FILE *stream) {
     if (c == '\n') {
         uart_putchar('\r', stream);
@@ -56,10 +48,9 @@ static int uart_putchar(char c, FILE *stream) {
     tx_head = next_head;
     USART0.CTRLA |= USART_DREIE_bm;
     insomnia_mask |= INSOMNIA_DEBUG_TX;
+    return 0;
 }
-#endif
 
-#ifdef DEBUG
 void debug_printf(const char *fmt, ...) {
     va_list args;
     printf("[%u] ", rtc_get_ticks());
@@ -67,11 +58,6 @@ void debug_printf(const char *fmt, ...) {
     vprintf(fmt, args);
     va_end(args);
 }
-#else
-void debug_printf(const char *fmt, ...) {
-    (void)fmt;
-}
-#endif
 
 ISR(USART0_DRE_vect) {
 	if (tx_tail != tx_head) {
@@ -92,3 +78,18 @@ ISR(USART0_TXC_vect) {
     USART0.STATUS |= USART_TXCIF_bm; // Clear interrupt flag
 }
 
+#else
+// Dummy implementations when DEBUG is not defined
+void debug_init(void) {
+    // No-op
+}
+
+char debug_read_char(void) {
+    return 0;
+}
+
+void debug_printf(const char *fmt, ...) {
+    (void)fmt;
+}
+
+#endif
